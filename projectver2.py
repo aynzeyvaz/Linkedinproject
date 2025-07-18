1435cc430f55457e17023e2f27cd525178ce7a80
Aynaz Eyvazzadeh <aynazeivazzadeh@gmail.com>
2025-07-18 13:29:36 +0800

    there is an issue with number again

--- projectver2.py
+++ projectver2.py
@@ -2,13 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys 
 from selenium.webdriver.firefox.options import Options
+
+
 import time
 import pandas as pd
 
 
 jobtitle=input("What job title do u want to search for? ")
 location=input("Which location? ")
-number=int(input("How many results would u like to see? "))    #the number does not work correctly
+number=int(input("How many results would u like to see? "))  
 
 job=jobtitle.replace(" ", "%20").lower()
 loc=location.replace(" ", "%20").replace(",", "%2c").lower()
@@ -19,6 +21,7 @@ driver=webdriver.Firefox(options=options)
 link=f"https://www.linkedin.com/jobs/search/?keywords={job}&location={loc}"
 driver.get(link)
 time.sleep(3)
+
 height=driver.execute_script("return document.body.scrollHeight;")      #JS code that returns the height of the page
 max=10
 n=0
@@ -31,25 +34,27 @@ while True:                                               #i think there might b
       driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
       time.sleep(3)
       n+=1
-      
 
-
-count=0
-list=[]
+   
 titles=[]
 companies=[]
 locations=[]
 links=[]
 for j in jobs[:number]:
-    title=j.find_element(By.CLASS_NAME, "base-search-card__title").text.strip()
-    company=j.find_element(By.CLASS_NAME, "base-search-card__subtitle").text.strip()
-    location=j.find_element(By.CLASS_NAME, "job-search-card__location").text.strip()
-    linktojob=j.find_element(By.TAG_NAME, "a").get_attribute("href").strip()
-    if title and company and location and linktojob:
-         titles.append(title)
-         companies.append(company)
-         locations.append(location)
-         links.append(linktojob)
+    try:
+        title=j.find_element(By.CLASS_NAME, "base-search-card__title").text.strip()
+        company=j.find_element(By.CLASS_NAME, "base-search-card__subtitle").text.strip()
+        location=j.find_element(By.CLASS_NAME, "job-search-card__location").text.strip()
+        linktojob=j.find_element(By.TAG_NAME, "a").get_attribute("href").strip()
+        if title and company and location and linktojob:
+          titles.append(title)
+          companies.append(company)
+          locations.append(location)
+          links.append(linktojob)
+    except:
+         continue
+      
+       
   
 table=pd.DataFrame({ "Title": titles, "Company":companies, "Location": locations, "Link": links})
 print(table.to_string())
