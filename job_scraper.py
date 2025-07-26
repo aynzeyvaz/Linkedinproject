798b126b6a4c6a3f4f835259d20d0c7a3ac993f7
Aynaz Eyvazzadeh <aynazeivazzadeh@gmail.com>
2025-07-26 13:07:53 +0800

    oop version, headless, file saved in the same dir

--- job_scraper.py
+++ job_scraper.py
@@ -4,52 +4,73 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
 import time
 import pandas as pd
+import os
+ 
+
+class linkedin():
+    def __init__(self, jobtitle, location, number):
+        self.jobtitle=jobtitle
+        self.location=location
+        self.number=number
+    
+    def create_driver(self):
+        options=Options()
+        options.add_argument("--headless")
+        self.driver=webdriver.Firefox(options=options)
+
+    def search_URL(self):
+        self.job=self.jobtitle.replace(" ", "%20").lower()
+        self.loc=self.location.replace(" ", "%20").replace(",", "%2c").lower()
+        self.link=f"https://www.linkedin.com/jobs/search/?keywords={self.job}&location={self.loc}"
+        self.driver.get(self.link)
+        time.sleep(3)
+
+    def job_scraper(self):
+        height=self.driver.execute_script("return document.body.scrollHeight;")
+        self.jobs=[]
+
+        for i in range(5):                                               
+            self.jobs.extend(self.driver.find_elements(By.CLASS_NAME, "base-card"))
+            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(3)
+            newheight=self.driver.execute_script("return document.body.scrollHeight;")
+            if newheight==height:
+                break
+            height=newheight
+        
+        titles=[]
+        companies=[]
+        locations=[]
+        links=[]
+        for j in self.jobs[:self.number]:
+            title=j.find_element(By.CLASS_NAME, "base-search-card__title").text.strip()
+            company=j.find_element(By.CLASS_NAME, "base-search-card__subtitle").text.strip()
+            location=j.find_element(By.CLASS_NAME, "job-search-card__location").text.strip()
+            linktojob=j.find_element(By.TAG_NAME, "a").get_attribute("href").strip()
+            if title and company and location and linktojob:
+                titles.append(title)
+                companies.append(company)
+                locations.append(location)
+                links.append(linktojob)
+        
+        table=pd.DataFrame({ "Title": titles, "Company":companies, "Location": locations, "Link": links})
+        table.dropna(inplace=True)
+        table.drop_duplicates(inplace=True)
+       
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        filepath = os.path.join(current_dir,"results.csv")
+        table.to_csv(filepath, index=False)
+        self.driver.quit()
+
 
 
 jobtitle=input("What job title do u want to search for? ")
 location=input("Which location? ")
-number=int(input("How many results would u like to see? "))             
-
-job=jobtitle.replace(" ", "%20").lower()
-loc=location.replace(" ", "%20").replace(",", "%2c").lower()
-
-#options=Options()
-#options.add_argument("--headless")
-driver=webdriver.Firefox() #options=options
-link=f"https://www.linkedin.com/jobs/search/?keywords={job}&location={loc}"
-driver.get(link)
-time.sleep(3)
+number=int(input("How many results would u like to see? "))
+start=linkedin(jobtitle,location,number)
+start.create_driver()
+start.search_URL()
+start.job_scraper()
 
 
-height=driver.execute_script("return document.body.scrollHeight;")    
-jobs=[]
-for i in range(5):                                               
-     jobs.extend(driver.find_elements(By.CLASS_NAME, "base-card"))
-     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-     time.sleep(3)
-     newheight=driver.execute_script("return document.body.scrollHeight;")
-     if newheight==height:
-          break
-     height=newheight
-      
-titles=[]
-companies=[]
-locations=[]
-links=[]
-
-for j in jobs[:number]:
-    title=j.find_element(By.CLASS_NAME, "base-search-card__title").text.strip()
-    company=j.find_element(By.CLASS_NAME, "base-search-card__subtitle").text.strip()
-    location=j.find_element(By.CLASS_NAME, "job-search-card__location").text.strip()
-    linktojob=j.find_element(By.TAG_NAME, "a").get_attribute("href").strip()
-    if title and company and location and linktojob:
-         titles.append(title)
-         companies.append(company)
-         locations.append(location)
-         links.append(linktojob)
-  
-table=pd.DataFrame({ "Title": titles, "Company":companies, "Location": locations, "Link": links})
-table.dropna(inplace=True)
-table.drop_duplicates(inplace=True)
-table.to_csv("C:\\Users\\Vivo\\Desktop\\Folders\\FinalProject\\results.csv")
  
