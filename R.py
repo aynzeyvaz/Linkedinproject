6ca6a8458cedaa3fe45d3eda7845134a3095becc
Aynaz Eyvazzadeh <aynazeivazzadeh@gmail.com>
2025-08-14 15:38:16 +0800

    Vallahi this is the final version

--- R.py
+++ R.py
@@ -7,5 +7,11 @@ import pandas as pd
 start=Gui()
 start.form()
 if start.sub_button:
-    start.searcher()
-    start.show()
+    if start.jobtitle:
+        if (start.location_type=="Other" and start.location) or start.location_type=="Remote":
+            start.searcher()
+            start.show()
+        else:
+            st.warning("Enter a location!!!")
+    else:
+        st.warning("Enter a job title!!!")
